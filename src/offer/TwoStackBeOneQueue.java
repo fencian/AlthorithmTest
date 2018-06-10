e55aa8d3561638d5c968f8a25b086c4da5a3b252
fencian <https://github.com/fencian/hiworld>
2018-06-10 15:45:40 +0800

    first commit

--- src/offer/TwoStackBeOneQueue.java
+++ src/offer/TwoStackBeOneQueue.java
@@ -6,7 +6,7 @@ public class TwoStackBeOneQueue {
     Stack<Integer> stack2 = new Stack<Integer>();
 
     public void push(int node) {
-
+        System.out.println("whatever");
     }
 
     public int pop() {
